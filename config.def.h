b21f7599d9e2d0aa9bdb309d9e915d61ca0d9fca
Akshay Nair <phenax5@gmail.com>
2020-05-25 19:32:32 +0530

    Adds and fixes fuzzyhighlighting pointer issue

--- config.def.h
+++ config.def.h
@@ -13,6 +13,8 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeSelHighlight] = { "#ffc978", "#005577" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
